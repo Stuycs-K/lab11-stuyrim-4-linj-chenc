70cf55c80f38a39d542d82d6117a94cd4416d15b
Joyce Lin <jlin60@stuy.edu>
2025-01-09 10:00:22 -0600

    Added skeleton for priest and accessor/constructors

--- Priest.java
+++ Priest.java
@@ -1,3 +1,50 @@
 public class Priest extends Adventurer {
+  int faith, faithMax;
 
+  public Priest(String name, int hp){
+    super(name,hp);
+    faithMax = 12;
+    faith = faithMax;
+  }
+
+  public Priest(String name){
+    this(name,30);
+  }
+
+  public Priest(){
+    this("Léopoldine Goyathlay");
+  }
+
+  public String getSpecialName(){
+    return "faith";
+  }
+
+  public int getSpecial(){
+    return faith;
+  }
+
+  public void setSpecial(int n){
+    faith = n;
+  }
+
+  public int getSpecialMax(){
+    return faithMax;
+  }
+
+  public String attack(Adventurer other){
+
+  }
+
+  public String specialAttack(Adventurer other){
+
+  }
+
+  public String support(Adventurer other){
+
+  }
+
+  public String support(){
+
+  }
+  
 }
